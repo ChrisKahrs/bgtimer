2b64062dc5765736015531b1e16d22b7ba404b05
Chris Kahrs <chriskahrs@msn.com>
2023-04-15 15:02:31 -0400

    forms working

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Api
 
 from .profiles import profiles_blueprint
@@ -14,3 +14,10 @@ api.add_resource(ProfileResource, '/profiles')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/read_form', methods=['GET', 'POST'])
+def read_form():
+    data = request.form
+    x = data['gamename']
+    print("data_room: ",x)
+    return render_template('index2.html')
